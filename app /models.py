e231be9dc90ff0b5f59f0d9792384fbe7969a7d2
muli3203 <muli3203@gmail.com>
2020-02-20 06:26:32 +0300

    Create classes blog comment and user .did some password hashing to hide the passwords and did relaionships on the db model

---
+++ app /models.py
@@ -0,0 +1,83 @@
+from . import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+
+    blogs = db.relationship("Blog", backref="user", lazy="dynamic")
+    comments = db.relationship("Comment", backref="user", lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError("You cannot read the password attribute")
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f"User {self.username}"
+
+
+class Blog(db.Model):
+    """
+    Blog class to define blog Objects
+    """
+
+    __tablename__ = "blogs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    comments = db.relationship("Comment", backref="blog", lazy="dynamic")
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_all_blogs(cls):
+        """
+        Function that queries the database and returns all the blogs
+        """
+        blogs = Blog.query.all()
+
+        return blogs
+
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, id):
+        comments = Comment.query.filter_by(blog_id=id).all()
+
+        return comments
